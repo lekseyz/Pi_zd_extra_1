c80880cd02e0570cb0dc9c325ff5f4bed5f11fd7
Aleksey <sazykin@sfedu.ru>
2023-12-14 21:17:12 +0300

    tests

--- src/main.rs
+++ src/main.rs
@@ -5,3 +5,16 @@ fn divByOne(a: u32) -> u32 {
 fn main() {
     println!("grate answer: {}", divByOne(42)); 
 }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(divByOne(42), 42);
+        assert_eq!(divByOne(4), 4);
+        assert_eq!(divByOne(2), 2);
+    }
+}
